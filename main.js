c0714f424df3c15897d5179e707609ab477034b2
abhijitrayarao <abhijitrayarao@gmail.com>
2022-03-19 15:58:47 +0530

    avk

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ const Minutesa = document.querySelector(".minutes h3");
 const secondsa = document.querySelector(".seconds h3");
 const body = document.querySelector("body");
 const register =document.querySelector(".register")
+const heading = document.querySelector(".register-section-heading h3");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 dropdDown.addEventListener('click' ,()=>{
   dropDownitems.classList.toggle("display")
   dropdDown.classList.toggle("boxShadow");
@@ -26,6 +29,8 @@ hamburger.addEventListener('click',()=>{
    navLinks.classList.toggle("transformx");
    body.classList.toggle("overflowY")
    register.classList.toggle("z-index");
+  //  nextBtn.classList.toggle("z-index");
+  //  prevBtn.classList.toggle("z-index");
 })
 function changeDate(){
  const currentYear =  new Date()
@@ -50,7 +55,127 @@ function timmer(){
   hoursa.innerText = hoursLeft;
   Minutesa.innerText = minutesLeft;
   secondsa.innerText = secondsLeft;
-     
  }
 }
 setInterval(timmer,1000)
+const headingCarosoul1 = ()=>{
+  heading.innerText = "AVISHKAR";
+  register.classList.toggle("opacity");
+}
+const headingCarosoul2 = ()=>{
+  heading.innerText = "The most awaited event is here";
+  register.classList.remove("opacity");
+}
+function Carosoul(){
+  // setTimeout(headingCarosoul1,4000);
+  setInterval(headingCarosoul2,5000);
+  setInterval(headingCarosoul1,10000) 
+}
+Carosoul();
+// setInterval(Carosoul ,5500)
+
+let  currentincrement = 0;
+nextBtn.addEventListener('click',()=>{
+    currentincrement++;
+   let count=currentincrement;
+    if(count%2 === 0){
+      headingCarosoul1();
+    }                
+    else{
+      headingCarosoul2();
+    }
+  });
+  prevBtn.addEventListener('click',()=>{
+    currentincrement++;
+   let count=currentincrement;
+    console.log(count);
+    if(count%2 === 0){
+      headingCarosoul1();
+    }                
+    else{
+      headingCarosoul2();
+      
+    }
+  });
+  const active =document.querySelectorAll(".dots li")
+  const testcontent1 = document.querySelector(".mb1");
+  const testcontent2 = document.querySelector(".mb3");
+  const testcontent3 = document.querySelector(".mb2");
+  // const testcontent1 = document.querySelector(".mb4");
+  const testimg1 = document.querySelector(".testimg1");
+  const testimg2 = document.querySelector(".testimg2");
+  const testimg3 = document.querySelector(".testimg3");
+  const testname1 = document.querySelector(".testimonial__name1")
+  const testname2 = document.querySelector(".testimonial__name2")
+  const testname3 = document.querySelector(".testimonial__name3")
+  const testjob1 = document.querySelector(".testimonial__job1");
+  const testjob2 = document.querySelector(".testimonial__job2");
+  const testjob3 = document.querySelector(".testimonial__job3");
+  function changetest1() {
+    testcontent1.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg1.src = "https://source.unsplash.com/1600x1600/?water";
+    testname1.innerText = "ABHIJT";
+    active.forEach(element=>{
+      element.classList.toggle("active");
+    });
+  }
+  function changetest2() {
+    testcontent2.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg2.src = "https://source.unsplash.com/1600x1600/?photo";
+    testname2.innerText = "ABHIJT";
+    active.forEach(element=>{
+      element.classList.replace("active","notactive");
+    });
+  }
+  function changetest3() {
+    testcontent3.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg3.src = "https://source.unsplash.com/1600x1600/?profile";
+    testname3.innerText = "ABHIJT";
+    active.forEach(element=>{
+      element.classList.toggle("active");
+    });
+  }
+  function changetest11() {
+    testcontent1.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg1.src = "https://source.unsplash.com/1600x1600/?water";
+    testname1.innerText = "rayarao";
+    active.forEach(element=>{
+      element.classList.toggle("active");
+    });
+  }
+  function changetest12() {
+    testcontent2.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg2.src = "https://source.unsplash.com/1600x1600/?wildlife";
+    testname2.innerText = "rayarao";
+    active.forEach(element=>{
+      element.classList.toggle("active");
+    });
+  }
+  function changetest13() {
+    testcontent3.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    testimg3.src = "https://source.unsplash.com/1600x1600/?sky";
+    testname3.innerText = "rayarao";
+    active.forEach(element=>{
+      element.classList.toggle("active");
+    });
+  }
+  function changeTestmonies(){
+    setInterval(changetest11,5000)
+    setInterval(changetest12,5000)
+    setInterval(changetest13,5000)
+    setInterval(changetest1,10000)
+    setInterval(changetest2,10000)
+    setInterval(changetest3,10000)
+    // changetest2();
+    // changetest3();
+  }
+  changeTestmonies();
+   // window.addEventListener('load',()=>{
+  //   const p = document.createElement("p");
+  //   p.innerText = "loading....."
+  //   p.appendChild(nextBtn);
+  // })
+
+// function testChange() {
+//   test.inner
+// }
